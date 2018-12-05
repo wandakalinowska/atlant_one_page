61d570d525bc046e18bd7b0b7e874bdadd512c5e
Wanda Kalinowska <wanda0501@gmail.com>
2018-12-05 22:41:22 +0100

    Add filter portfolio

---
+++ scripts/filter_script.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(".category-button").click(function(){var filterValue=$(this).attr('data-filter');if(filterValue=="all"){$(".all").show("slow");}else{$(".all").not('.'+filterValue).hide("slow");$(".all").filter('.'+filterValue).show("slow");}});});
